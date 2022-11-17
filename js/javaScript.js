dc476546940cb3ed31a9f7eeea3d547cab446aa7
Aleksander Borovikov <alexander456@gmail.com>
2022-11-17 11:53:16 +0300

    js сделал закрытие вклада

---
+++ js/javaScript.js
@@ -0,0 +1,131 @@
+const buttonInsert = document.getElementById("insert");
+document.getElementById("FormPersonData").addEventListener("submit", update);
+document.getElementById("FormMoney").addEventListener("submit", update);
+let nameContribution = document.getElementById("name-contribution");
+let typeContribution = document.getElementById("type-contribution");
+const newContribution = document.querySelector(".new-contribution");
+document.getElementById("open-score").addEventListener("click", () => {check();});
+let sumContribution = document.getElementById("sum-contribution");
+document.getElementById("top-up").addEventListener("click", () => {
+  introductionSum(infoContribution,nameContribution.value,sumContribution.value);});
+document.getElementById("take-off").addEventListener("click", () => {
+  withdrawalSum(infoContribution,nameContribution.value,sumContribution.value);
+});
+const not = document.getElementById("not");
+let infoContribution = [];
+
+function update(event) {
+  event.preventDefault();
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+  const PersonDataButton = document.getElementById("PersonDataButton");
+  const formShow = document.getElementById("form-show");
+  PersonDataButton.addEventListener("click", () => {
+    openForm(formShow, "open", "closed", 500);
+  });
+});
+
+function openForm(element, open, closed, timer) {
+  if (!element.classList.contains(open)) {
+    element.classList.add(open);
+    element.classList.remove(closed);
+  } else {
+    element.classList.add(closed);
+    window.setTimeout(function () {
+      element.classList.remove(open);
+    }, timer);
+  }
+}
+
+buttonInsert.addEventListener("click", () => {
+  let nameUser = document.getElementById("name").value;
+  let Surname = document.getElementById("Surname").value;
+  let birth = document.getElementById("Year-of-birth").value;
+  document.getElementById("user-name").innerHTML = nameUser;
+  document.getElementById("user-surname").innerHTML = Surname;
+  document.getElementById("user-birth").innerHTML = birth;
+});
+
+function check() {
+  if (nameContribution.value === "" || parseInt(nameContribution.value) < 16) {
+    alert("Проверьте правильность написания счета!");
+  } else {
+    let logics = conformity(infoContribution, nameContribution.value);
+    if (logics) {
+      alert("Данный счет уже открыт!");
+    } else {
+      openDeposits(newContribution);
+    }
+  }
+}
+
+function Random(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function openDeposits(el) {
+  let balance = 0;
+  let rnd = Random("1", "10000");
+  el.innerHTML += `<div><div>номер вклада:
+    ${nameContribution.value}</div class="contr"> <div>тип вклада:${typeContribution.value}</div>
+    <div>баланс:<span id="${rnd}">${balance}</span></div></div>`;
+  newObject(infoContribution, nameContribution.value, balance, rnd);
+}
+
+function newObject(array, room, balance, rnd) {
+  let contribution = {
+    idContribution: parseInt(room),
+    sum: parseInt(balance),
+    idSpan: rnd,
+  };
+  array.push(contribution);
+}
+
+function conformity(array, room) {
+  for (const arr of array) {
+    if (arr.idContribution === parseInt(room)) {
+      return true;
+    }
+  }
+}
+
+function checkSum(sum) {
+  let message = prompt("Введите трехзначный код");
+  if (message.length === 3 && message === "123") {
+    if (sum === "" || sum === "0") alert("Введите сумму");
+    else if (sum < "0") alert("Нельзя вводить отрицательное число!");
+    else return true;
+  } else alert("Неверный код");
+}
+
+function introductionSum(array, room, balance) {
+  for (const arr of array) {
+    if (arr.idContribution === parseInt(room)) {
+      if (checkSum(balance)) {
+        let span = document.getElementById(arr.idSpan);
+        arr.sum += parseInt(balance);
+        span.innerHTML = arr.sum;
+        not.innerHTML += `<div>Пополнение счета:${arr.idContribution}</div>
+				<div>на сумму:${balance} рублей</div><div>	баланс счета:${arr.sum}</div>`;
+      }
+    } 
+  }
+}
+
+function withdrawalSum(array, room, balance) {
+  for (const arr of array) {
+    if (arr.idContribution === parseInt(room)) {
+      if (checkSum(balance)) {
+        let value = parseInt(arr.sum) - parseInt(balance);
+        if (value >= 0) {
+          let span = document.getElementById(arr.idSpan);
+          span.innerHTML = value;
+          arr.sum -= parseInt(balance);
+          not.innerHTML += `<div>Снятие со счета:${arr.idContribution}</div>
+					<div>суммы:${balance} рублей</div><div> баланс счета:${arr.sum} рублей</div>`;
+        } else alert("Недостаточно средств для снятия");
+      }
+    } 
+  }
+}
