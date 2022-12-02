c021ce0d99276565fd718e92973e21de63e4bf92
CakeN1ck <rozhkonick22@gmail.com>
2022-12-02 18:06:11 +0300

    Починил

--- js/script.js
+++ js/script.js
@@ -1,34 +1,75 @@
 const walletsList = document.getElementById("wallets-list")
-const notificationList = document.getElementById("notification-list")
 const formOpenWallet = document.getElementById('form-open-wallet');
-const getWallets = document.getElementById('get-wallets');
+const formCloseWallet = document.getElementById('form-close-wallet');
+const walletAddMoney = document.getElementById('wallet-add-money');
+const walletWithdrawMoney = document.getElementById('wallet-withdraw-money');
 let  account = {}
+
 async function main() {
-    account = await load()
+    account = load()
     outputWallets(account.wallets)
-    outputNotifications(account.notifications)
 }
 
 main().then()
+
 formOpenWallet.addEventListener('submit', async (e) => {
     e.preventDefault();
     const wallet = {
         name: document.getElementById('name').value,
-        amount: document.getElementById('amount').value,
-        transactions: [],
+        amount: Number(document.getElementById('amount').value),
         id: random(1000000000000000, 9999999999999999),
-
     }
-    await sendNotification('Счет успешно создан', `Вы открыли новый счет под названием: ${name}`)
-
     await addWallet(wallet)
 })
 
-getWallets.addEventListener('click', async () => {
-    console.log(account.wallets)
-});
+formCloseWallet.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const id = Number(document.getElementById('id').value);
+    await closeWallet(id)
+})
+
+walletAddMoney.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const id = Number(document.getElementById('idAdd').value);
+    const amount = Number(document.getElementById('moneyAdd').value);
+    await addMoney(id, amount)
+})
+
+walletWithdrawMoney.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const id = Number(document.getElementById('idWithdraw').value);
+    const amount = -Number(document.getElementById('moneyWithdraw').value);
+    await addMoney(id, amount)
+})
+
+async function addMoney(id, amount) {
+    for (const wallet of account.wallets) {
+        if (wallet.id === id) {
+            wallet.amount = Number(wallet.amount) + Number(amount)
+            await save()
+            alert(`Баланс изменен`)
+            return outputWallets(account.wallets)
+        }
+    }
+    alert(`Банковский счет с таким id не найден`)
+}
+
+async function closeWallet(id) {
+    for (const wallet of account.wallets) {
+        if (wallet.id === id) {
+            const index = account.wallets.indexOf(wallet)
+            account.wallets.splice(index, 1)
+            await save()
+            alert(`Банковский счет закрыт`)
+            return outputWallets(account.wallets)
+        }
+    }
+    alert(`Банковский счет с таким id не найден`)
+}
 
 async function addWallet(newWallet) {
+    if (!account.wallets) account.wallets = []
+    if (isNaN(newWallet.amount)) return alert("Введите число")
     for (const wallet of account.wallets) {
         if (wallet.name === newWallet.name) {
             return alert(`У вас уже есть банковский счет с таким названием`)
@@ -37,9 +78,10 @@ async function addWallet(newWallet) {
     account.wallets.push(newWallet)
     await save()
     alert(`Открыт новый банковский счет ${newWallet.name}`)
+    outputWallets(account.wallets)
 }
 
-async function load() {
+function load() {
     let user = JSON.parse(localStorage.getItem(`account`))
     if (!user) {
         user = {
@@ -48,7 +90,6 @@ async function load() {
             photo: "main-user.png",
             dateOfBirth: new Date(2002, 11, 22),
             wallets: [],
-            notifications: [],
             gender: "male",
         }
     }
@@ -59,7 +100,6 @@ async function load() {
         <p>Фамилия: ${user.lastName}</p>
         <p>Пол: ${user.gender === "male" ? "Мужской" : "Женский"}</p>
 `
-    await save()
     return user;
 }
 
@@ -69,36 +109,17 @@ async function save() {
 function outputWallets(wallets) {
     walletsList.innerHTML = '';
     for (const wallet of wallets) {
-        let transactions = ``
-        for (const transaction of wallet.transactions) {
-            transactions += `
-            <div class="transaction">
-                <p>Тип транзакции: ${transaction.type === "get" ? "Пополнение" : "Списание"}</p>
-                <p>Сумма: ${transaction.money} $</p> 
-                <p>Дата перевода: ${transaction.date}</p>
-                <p>${transaction.type === "get" ? "Отправитель" : "Получатель"}: ${transaction.name}</p>
-                <p>Комментарий: ${transaction.comment}</p>
-            </div>
-            `
-        }
         walletsList.innerHTML += `
 <div class="card"">
     <div class="card-header">
         <h4>${wallet.name}</h4>
-        <h6>id: ${wallet.id}</h6>
-        <h6 class="${wallet.type}-card-text">Тип карты: ${wallet.type}</h6>
+        <h5>id: ${wallet.id}</h5>
     </div>
     
     <div class="card-text">
-        <p>Баланс: ${wallet.money} $</p>
+        <p>Баланс: ${wallet.amount} $</p>
     </div>
     
-    <div class="card-transactions">
-        <h5>Последние транзакции:</h5>
-       
-        ${transactions}
-        
-    </div>
 </div>
         `;
     }
@@ -108,61 +129,6 @@ function random(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function outputNotifications(notifications) {
-    notificationList.innerHTML = '';
-    for (const notification of notifications) {
-        notificationList.innerHTML += `
-<div class="notification-card"">
-    <div class="card-header">
-        <h4>${notification.name}</h4>
-    </div>
-    
-    <div class="card-text">
-        <p>${notification.text}</p>
-    </div>
-    
-    <div class="card-footer">
-        <p>${notification.date}</p>
-    </div>
-</div>
-        `;
-    }
-}
-
-async function sendNotification(name, text) {
-    createSections(account, "notifications")
-    account.notifications.push({
-        name: name,
-        text: text,
-        date: new Date()
-    })
-    await save()
-    outputNotifications(account.notifications)
-}
-
-async function transaction(type, name, user, money, comment) {
-    await sendNotification(`${type === "get" ? "Входящий" : "Исходящий"} перевод`, `
-Пользователь: ${user}
-<br> Перевел вам: ${money}$ 
-<br> На счет: ${name} 
-<br> Комментарии при переводе: "${comment}".`
-    )
-    for (const wallet of account.wallets) {
-        if (wallet.name === name) {
-            type === "get" ? wallet.money += money : wallet.money -= money
-            wallet.transactions.push({
-                type: type,
-                date: new Date(),
-                name: user,
-                comment: comment,
-                money: money
-            })
-            break
-        }
-    }
-    await save()
-    outputWallets(account.wallets)
-}
 function createSections(arr, name) {
     if (!arr[name]) arr[name] = []
 }
